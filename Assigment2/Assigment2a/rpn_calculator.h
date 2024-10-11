bb983f97ba4eb3cccc579721d1fe33db0cc179d2
AlekseyGarciaAdov <st135738@student.spbu.ru>
2024-10-11 11:06:16 +0300

    Uploading Assigment2a

---
+++ Assigment2/Assigment2a/rpn_calculator.h
@@ -0,0 +1,10 @@
+
+
+#ifndef TEXT_FILE_OPERATIONS_H
+#define TEXT_FILE_OPERATIONS_H
+
+#include <string>
+
+void reverseTextFile(const std::string& inputFile, const std::string& outputFile);
+
+#endif
